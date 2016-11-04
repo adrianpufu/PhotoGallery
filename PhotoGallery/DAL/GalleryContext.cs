5f7ff24b0063a347e9fb080c80d8ab09c2cd59f2
adrianpufu <adrianpufu1@gmail.com>
2016-11-05 00:08:13 +0200

    photo model created

---
+++ PhotoGallery/DAL/GalleryContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using PhotoGallery.Models;
+
+namespace PhotoGallery.DAL
+{
+    public class GalleryContext : DbContext
+    {
+        public GalleryContext() : base("GalleryContext")
+        {
+        }
+        public DbSet<Photo> Photos { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ PhotoGallery/DAL/GalleryInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using PhotoGallery.Models;
+
+namespace PhotoGallery.DAL
+{
+    public class GalleryInitializer : System.Data.Entity.
+    DropCreateDatabaseIfModelChanges<GalleryContext>
+    {
+        protected override void Seed(GalleryContext context)
+        {
+            var photos = new List<Photo>
+            {
+            new Photo{Name="Carson1",Description="Alexander1",Path="../photos/photo1.jpg"},
+            new Photo{Name="Carson2",Description="Alexander2",Path="../photos/photo2.jpg"},
+            new Photo{Name="Carson3",Description="Alexander3",Path="../photos/photo3.jpg"},
+            new Photo{Name="Carson4",Description="Alexander4",Path="../photos/photo4.jpg"},
+            new Photo{Name="Carson5",Description="Alexander5",Path="../photos/photo5.jpg"}
+            };
+            photos.ForEach(s => context.Photos.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
